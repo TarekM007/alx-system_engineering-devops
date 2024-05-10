f40ed6040e971dcb2aa969175b08d3cacf1ace22
TarekM007 <tarekmoustafa36@gmail.com>
2024-05-09 19:54:03 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-""" How many subs? """
-
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data["data"]["subscribers"]
-            return subscribers
-    except requests.exceptions.RequestException:
-        pass
-
-    return 0  # Return 0 if an error occurs or the subreddit is invalid
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
